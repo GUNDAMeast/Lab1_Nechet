6c12b66efead98bba99558970577f429b03747e3
GUNDAMeast <strelnikov.maxim2004@gmail.com>
2025-10-22 21:38:21 +0500

    Add files via upload

---
+++ SMV-Lab1-Nechet/InputCheck.cs
@@ -0,0 +1,10 @@
+﻿namespace SMV_Lab1_Nechet
+{
+    class InputCheck
+    {
+        //это нам понадобится позже
+
+        //CharIntConvertible
+        //if (x >= '0' && x <= '9') return x - '0'
+    }
+}
