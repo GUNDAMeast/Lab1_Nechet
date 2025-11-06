1464c835b0251e65d62231ae7da483d3e0b78988
GUNDAMeast <strelnikov.maxim2004@gmail.com>
2025-11-06 21:41:19 +0500

    Add files via upload

---
+++ SMV-Lab1-Nechet/SMV-Lab1-Nechet/Lab1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SMV_Lab1_Nechet
+{
+    class Lab1
+    {
+        //1.1
+        public double Fraction(double x)
+        {
+            return x - (int)x;
+            //return (x - Math.Truncate(x));
+        }
+
+        //1.3
+        public int CharToNum(char x)
+        {
+            return (int)x - '0';
+            //return (int)x - 48;
+        }
+
+        //1.5
+        public bool Is2Digits(int x)
+        {
+            return ((x > 9) & (x < 100)) || ((x<-9) & (x>-100));
+        }
+
+        //1.7
+        public bool IsInRange(int a, int b, int num)
+        {
+            int left = Math.Min(a,b);
+            int right = Math.Max(a,b);
+            return (num >= left) & (num <= right);
+        }
+
+        //1.9
+        public bool IsEqual(int a, int b, int c)
+        {
+            return ((a == b) & (a == c));
+        }
+
+        //2.1
+        public int Abs(int x)
+        {
+            if (x < 0) x = -x;
+            return x;
+        }
+
+        //2.3
+        public bool Is35(int x)
+        {
+            return (x%3==0) ^ (x%5==0);
+        }
+
+        //2.5
+        public int Max3(int x, int y, int z)
+        {
+            if (x < y) x = y;
+            if (x < z) x = z;
+            return x;
+            //return Math.Max(Math.Max(x, y), z);
+        }
+
+        //2.7
+        public int Sum2(int x, int y)
+        {
+            int sum = x+y;
+            if ((sum>9) & (sum<20)) return 20;
+            return sum;
+        }
+
+        //2.9
+        public string Day(int x)
+        {
+            switch(x)
+                {
+                case 1:
+                    return "Понедельник";
+                case 2:
+                    return "Вторник";
+                case 3:
+                    return "Среда";
+                case 4:
+                    return "Четверг";
+                case 5:
+                    return "Пятница";
+                case 6:
+                    return "Суббота";
+                case 7:
+                    return "Воскресение";
+                default:
+                    return "Это не день недели";
+                }
+        }
+
+        //3.1
+        public string ListNums(int x)
+        {
+            string line = "";
+            if (x>=0)
+                for (int i=0;i<=x;i++)
+                {
+                    line = line + Convert.ToString(i)+ " ";
+                }
+            else
+                for (int i = 0; i >= x; i--)
+                {
+                    line = line + Convert.ToString(i) + " ";
+                }
+            return line.Trim();
+        }
+
+        //3.3
+        public string Chet(int x)
+        {
+            string line = "";
+            if (x>0)
+                for (int i=0;i<=x;i=i+2)
+                {
+                    line = line + Convert.ToString(i)+ " ";
+                }
+            else
+                for (int i = 0; i >= x; i = i - 2)
+                {
+                    line = line + Convert.ToString(i) + " ";
+                }
+            return line.Trim();
+        }
+
+        //3.5
+        public int NumLen(long x)
+        {
+            if (x == 0) return 1;
+            else
+            {
+                int count = 0;
+                while (x != 0)
+                {
+                    count++;
+                    x = x / 10;
+                }
+                return count;
+            }
+        }
+
+        //3.7
+        public void Square(int x)
+        {
+            for (int i=0;i<x;i++)
+            {
+                string s = new string('*', x);
+                Console.WriteLine(s);
+            }
+        }
+
+        //3.9
+        public void RightTriangle(int x)
+        {
+            int spaces = x;
+            for (int i=1;i<=x;i++)
+            {
+                spaces--;
+                string s = new string(' ', spaces);
+                string k = new string('*', i);
+                Console.WriteLine($"{s}{k}");
+            }
+        }
+
+        //4.1
+        public int FindFirst(int[] arr, int x)
+        {
+            for (int i=0; i<arr.Length;i++)
+            {
+                if (arr[i] == x) return i;
+            }
+            return -1;
+        }
+
+        //4.3
+        public int MaxAbs(int[] arr)
+        {
+            int most = Math.Abs(arr[0]);
+            for (int i = 1;i<arr.Length;i++)
+            {
+                if (Math.Abs(arr[i]) > most) most = Math.Abs(arr[i]);
+            }
+            return most;
+        }
+        
+        //4.5
+        public int[] Add(int[] arr, int[] ins, int pos)
+        {
+            int[] summas = new int[arr.Length + ins.Length];
+            for (int i=0; i<pos; i++)
+            {
+                summas[i] = arr[i];
+            }
+            for (int i=0; i<ins.Length;i++)
+            {
+                summas[pos + i] = ins[i];
+            }
+            for (int i=pos;i<arr.Length;i++)
+            {
+                summas[ins.Length + i] = arr[i];
+            }
+            return summas;
+        }
+        
+        //4.7
+        public int[] ReverseBack(int[] arr)
+        {
+            int[] mas = new int[arr.Length];
+            for (int i=0; i<arr.Length;i++)
+            {
+                mas[i] = arr[arr.Length-1 - i];
+            }
+            return mas;
+        }
+
+        //4.9
+        public int[] FindAll(int[] arr, int x)
+        {
+            int count = 0;
+            for (int i=0;i<arr.Length;i++)
+            {
+                if (arr[i] == x) count++;
+            }
+            if (count == 0) Console.WriteLine("Данное число не входит в этот массив");
+            int[] findmas = new int[count];
+            count = 0;
+            for (int i = 0; i<arr.Length;i++)
+            {
+                if (arr[i] == x)
+                {
+                    findmas[count] = i;
+                    count++;
+                }
+            }
+            return findmas;
+        }
+    }
+}
---
+++ SMV-Lab1-Nechet/SMV-Lab1-Nechet/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace SMV_Lab1_Nechet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var InputCheck = new InputCheck();
+            var Lab1 = new Lab1();
+
+            bool stopTasks = false;
+            string answer;
+
+            int k = 1;
+            string[][] tasksArray = new string[4][];
+            for (int i = 0; i<tasksArray.Length; i++)
+            {
+                tasksArray[i] = new string[5];
+                k = 1;
+                for (int j = 0; j < tasksArray[i].Length; j++)
+                {
+                    tasksArray[i][j] = (i + 1) + "." + k;
+                    k = k + 2;
+                }
+            }
+
+            while (stopTasks == false)
+            {
+                Console.WriteLine("\nКакое задание выполнить?");
+                for (int i = 0; i < tasksArray.Length; i++)
+                {
+                    for (int j = 0; j < tasksArray[i].Length; j++)
+                        Console.Write(tasksArray[i][j] + "    ");
+                    Console.WriteLine();
+                }
+                Console.WriteLine("Вы можете прекратить выполнение заданий если введёте 0.");
+                answer = Console.ReadLine();
+                Console.Clear();
+
+                switch (answer)
+                {
+                    case "0":
+                        stopTasks = true;
+                        break;
+                    
+                    case "1.1":
+                        Console.WriteLine("\n1.1 - Дробная часть");
+                        Console.WriteLine("Результат: " + Lab1.Fraction(InputCheck.CheckDouble()));
+                        break;
+                    case "1.3":
+                        Console.WriteLine("\n1.3 - Букву в число");
+                        Console.WriteLine("Результат: " + Lab1.CharToNum(InputCheck.CheckNumChar()));
+                        break;
+                    case "1.5":
+                        Console.WriteLine("\n1.5 - Двузначное");
+                        Console.WriteLine("Результат: " + Lab1.Is2Digits(InputCheck.CheckInt()));
+                        break;
+                    case "1.7":
+                        Console.WriteLine("\n1.7 - Диапазон");
+                        Console.WriteLine("Результат: " + Lab1.IsInRange(InputCheck.CheckInt(), InputCheck.CheckInt(), InputCheck.CheckInt()));
+                        break;
+                    case "1.9":
+                        Console.WriteLine("\n1.9 - Равенство");
+                        Console.WriteLine("Результат: " + Lab1.IsEqual(InputCheck.CheckInt(), InputCheck.CheckInt(), InputCheck.CheckInt()));
+                        break;
+
+                    case "2.1":
+                        Console.WriteLine("\n2.1 - Модуль числа");
+                        Console.WriteLine("Результат: " + Lab1.Abs(InputCheck.CheckInt()));
+                        break;
+                    case "2.3":
+                        Console.WriteLine("\n2.3 - Тридцать пять");
+                        Console.WriteLine("Результат: " + Lab1.Is35(InputCheck.CheckInt()));
+                        break;
+                    case "2.5":
+                        Console.WriteLine("\n2.5 - Тройной максимум");
+                        Console.WriteLine("Результат: " + Lab1.Max3(InputCheck.CheckInt(), InputCheck.CheckInt(), InputCheck.CheckInt()));
+                        break;
+                    case "2.7":
+                        Console.WriteLine("\n2.7 - Двойная сумма");
+                        Console.WriteLine("Результат: " + Lab1.Sum2(InputCheck.CheckInt(), InputCheck.CheckInt()));
+                        break;
+                    case "2.9":
+                        Console.WriteLine("\n2.9 - День недели");
+                        Console.WriteLine("Результат: " + Lab1.Day(InputCheck.CheckInt()));
+                        break;
+                    
+                    case "3.1":
+                        Console.WriteLine("\n3.1 - Числа подряд");
+                        Console.WriteLine("Результат: " + Lab1.ListNums(InputCheck.CheckInt()));
+                        break;
+                    case "3.3":
+                        Console.WriteLine("\n3.3 - Чётные числа");
+                        Console.WriteLine("Результат: " + Lab1.Chet(InputCheck.CheckInt()));
+                        break;
+                    case "3.5":
+                        Console.WriteLine("\n3.5 - Длина числа");
+                        Console.WriteLine("Результат: " + Lab1.NumLen(InputCheck.CheckLong()));
+                        break;
+                    case "3.7":
+                        Console.WriteLine("\n3.7 - Квадрат");
+                        Lab1.Square(InputCheck.CheckIntPlus());
+                        break;
+                    case "3.9":
+                        Console.WriteLine("\n3.9 - Правый треугольник");
+                        Lab1.RightTriangle(InputCheck.CheckIntPlus());
+                        break;
+                    
+                    case "4.1":
+                        Console.WriteLine("\n4.1 - Поиск первого значения");
+                        Console.WriteLine("Результат: " + Lab1.FindFirst(InputCheck.CheckIntMas(), InputCheck.CheckInt()));
+                        break;
+                    case "4.3":
+                        Console.WriteLine("\n4.3 - Поиск максимального");
+                        Console.WriteLine("Результат: " + Lab1.MaxAbs(InputCheck.CheckIntMas()));
+                        break;
+                    case "4.5":
+                        Console.WriteLine("\n4.5 - Добавление массива в массив");
+                        int[] x1 = InputCheck.CheckIntMas();
+                        int posInX1 = InputCheck.CheckIntPos(x1);
+                        InputCheck.PrintMas(Lab1.Add(x1, InputCheck.CheckIntMas(), posInX1));
+                        break;
+                    case "4.7":
+                        Console.WriteLine("\n4.7 - Возвратный реверс");
+                        InputCheck.PrintMas(Lab1.ReverseBack(InputCheck.CheckIntMas()));
+                        break;
+                    case "4.9":
+                        Console.WriteLine("\n4.9 - Все вхождения");
+                        InputCheck.PrintMas(Lab1.FindAll(InputCheck.CheckIntMas(), InputCheck.CheckInt()));
+                        break;
+                    
+                    default:
+                        Console.WriteLine("Нет такого задания!");
+                        break;
+                }
+            }
+            Console.WriteLine("Выполнение заданий было прекращено.");
+        }
+    }
+}
